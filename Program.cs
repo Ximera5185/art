0a1f1fdd9357476a8be6a8d3081c243ac3a74486
Ximera5185 <dmitrynikitenko@yandex.ru>
2023-09-08 19:16:05 +0800

    1

--- Program.cs
+++ Program.cs
@@ -10,9 +10,41 @@ namespace art
     {
         static void Main(string [] args)
         {
+        string messageOne = "Введите ваше имя:";
+        string messageTwo = "Введите вашу Фамилию:";
+         
+        string name = GetDataFromUser(messageOne); 
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Данные сохранил!");
+        Console.ResetColor();
+
+        
+        string surName = GetDataFromUser(messageTwo);
+        
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Данные сохранил!");
+        Console.ResetColor();
+
+        Console.WriteLine($"{name} {surName}");
+        }
+
+        public static void GetDataFromUser(string message;)
+        {
+            Console.WriteLine(message);
+
+            string dataFromUser = Console.ReadLine();
+
+            return dataFromUser;
+        }
+
+        public static void ChangeConsoleColor(ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
         }
     }
 }
+
 /*Задание 1:
 Избавься от дубляжа. Пока без класса, используя создание методов в классе Program.
     class Program
