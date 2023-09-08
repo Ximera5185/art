c2de4bf9822d11c9b8dc9af197c3e2dab4799779
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2023-09-08 13:41:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace art
+{
+    internal class Program
+    {
+        static void Main(string [] args)
+        {
+        }
+    }
+}
+/*Задание 1:
+Избавься от дубляжа. Пока без класса, используя создание методов в классе Program.
+    class Program
+{
+    static void Main(string [] args)
+    {
+        Console.WriteLine("Введите ваше имя: ");
+        string name = Console.ReadLine();
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Данные сохранил!");
+        Console.ResetColor();
+
+        Console.WriteLine("Введите вашу Фамилию: ");
+        string surName = Console.ReadLine();
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Данные сохранил!");
+        Console.ResetColor();
+
+        Console.WriteLine($"{name} {surName}");
+    }
+}*/
